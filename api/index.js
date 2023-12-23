ad250d6fae05f56cebc993d2ce5cb774d5261bf6
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-23 08:43:27 +0300

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to Database');
+}).catch((error) => {
+    console.log(error)
+});
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
-})
+});
