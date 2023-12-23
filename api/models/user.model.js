4487c4bcbd704a672b1ad705fc556fb9ce2c0159
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-23 08:53:19 +0300

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+},{timestamps: true})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
