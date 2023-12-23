007e987979423090c893f851610edb4ce4d2cbb9
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-23 09:04:47 +0300

    created test controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,7 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    res.json({
-        "message":"Api is working"
-    })
-});
-
+router.get('/test', test)
 export default router
